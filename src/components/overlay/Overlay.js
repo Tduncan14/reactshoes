ab949f5ee8c747e89d13a5ab0876d068a7026d98
tduncan14 <nbatre1@yahoo.com>
2022-07-13 04:17:12 -0700

    keep moving forward

--- src/components/overlay/Overlay.js
+++ src/components/overlay/Overlay.js
@@ -1,16 +1,26 @@
-import React ,{useState} from 'react';
+import React ,{useState,useEffect} from 'react';
 import './Overlay.css';
 import  Image1 from '../../asset/image-product-1.jpg';
 import  Image2 from '../../asset/image-product-2.jpg';
 import  Image3 from '../../asset/image-product-3.jpg';
 import  Image4 from '../../asset/image-product-4.jpg';
 import  Close from  '../../asset/icon-close.svg';
+import  Next from  '../../asset/icon-next.svg';
+import  Prev from  '../../asset/icon-previous.svg';
 
  const  Overlay = ({setTheOverlay,image})  => {
 
     const[mainImage,setmainImage] = useState(image);
+    const [arr,setArray] = useState([])
 
+  
     
+
+    useEffect(() =>{
+        window.scrollTo(20,20)
+
+
+    },[])
   
    const onChangeImage = (e) => {
     
@@ -19,12 +29,74 @@ import  Close from  '../../asset/icon-close.svg';
     setmainImage(e.target.src)
    }
 
+   const prev = (e) => {
+
+  
+    console.log(e.target,mainImage)
+    console.log(mainImage.match() == Image1)
+
+    const imagess = [Image1,Image2,Image3,Image4] ;
+
+
+    if(mainImage.match(Image1) == Image1){
+
+    }
+
+    if(mainImage.match(Image2) == Image2){
+       setmainImage(Image1)
+    }
+
+    if(mainImage.match(Image3) == Image3){
+      setmainImage(Image2)
+   }
+   
+   if(mainImage.match(Image4) == Image4){
+    setmainImage(Image3)
+ }
+ 
+    
+
+
+   }
+
+   const next = () => {
+
+    if(mainImage.match(Image1) == Image1){
+      setmainImage(Image2)
+    }
+
+    if(mainImage.match(Image2) == Image2){
+      setmainImage(Image3)
+
+    }
+
+    if(mainImage.match(Image3) == Image3){
+      setmainImage(Image4)
+   }
+  
+    
+  }
+
   return (
     <div className="overlay">
 
          
 
 <img onClick={setTheOverlay} className="close" src={Close}/>
+
+
+
+<div  onClick={prev} class="prev">
+<img  className="prev-arr" src={Prev}/>
+</div>
+
+<div onClick={next} className='next'>
+<img onClick className="next-arr" src={Next}/>
+</div>
+
+
+
+
          <div className="mainImag">
 
 <img src={mainImage}  alt="image" />
